941151001b59d1b1eae8f88fc13da2db2a862a53
ric9rdo <37312376+ric9rdo@users.noreply.github.com>
2020-04-22 10:28:57 -0500

    Add files via upload

---
+++ SwiftUIMenuBar/SwiftUIMenuBar/AppDelegate.swift
@@ -0,0 +1,50 @@
+//
+//  ContentView.swift
+//  SwiftUIMenuBar
+//
+//  Created by Ricardo Ribeiro on 05/04/2020.
+//  Copyright © 2020 Ricardo Ribeiro. All rights reserved.
+//
+import Cocoa
+import SwiftUI
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    var popover: NSPopover!
+    var statusBarItem: NSStatusItem!
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Create the SwiftUI view that provides the window contents.
+        let contentView = ContentView()
+
+        // Create the popover
+        let popover = NSPopover()
+        popover.contentSize = NSSize(width: 300, height: 350)
+        popover.behavior = .transient
+        popover.contentViewController = NSHostingController(rootView: contentView)
+        self.popover = popover
+        self.popover.contentViewController?.view.window?.becomeKey()
+        
+        // Create the status item
+        self.statusBarItem = NSStatusBar.system.statusItem(withLength: CGFloat(NSStatusItem.variableLength))
+        
+        if let button = self.statusBarItem.button {
+            button.image = NSImage(named: "Icon")
+            button.action = #selector(togglePopover(_:))
+        }
+        
+        NSApp.activate(ignoringOtherApps: true)
+    }
+    
+    @objc func togglePopover(_ sender: AnyObject?) {
+        if let button = self.statusBarItem.button {
+            if self.popover.isShown {
+                self.popover.performClose(sender)
+            } else {
+                self.popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
+            }
+        }
+    }
+    
+}
